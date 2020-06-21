01a393244d597ad74157fe05ae175201f2d4c8a4
Hannes_OnChuwi_lubuntu <hanneselias@gmail.com>
2020-06-21 21:29:10 +0200

    aaa

---
+++ lib/Widgets/Reuse/VideoBG.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class VideoBG extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //TODO: make this real video
+    return Container(
+      color: Theme.of(context).canvasColor,
+    );
+  }
+}
--- lib/pages/Frontpage.dart
+++ lib/pages/Frontpage.dart
@@ -1,10 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:meetby_accident/Widgets/MainDrawer.dart';
+import 'package:meetby_accident/Widgets/Reuse/VideoBG.dart';
 
 class Frontpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Stack(
+      children: <Widget>[
+        VideoBG(),
+        Container(
+          color: Theme.of(context).canvasColor.withAlpha(155),
+        ),
+        Column(
+          children: <Widget>[
+            ProfileRow(),
+          ]
+        ),
+      ],
+    );
+  }
+}
+
+class ProfileRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(40),
+      child: Row(
+        children: <Widget>[
+          ProfileIcon(),
+          //TODO: real name
+          Text("***Mein Name***", style: TextStyle(fontSize:22,color: Theme.of(context).textTheme.headline1.color),),
+        ],
+      ),
+    );
+  }
+}
+
+class ProfileIcon extends StatelessWidget {
+  double height;
+  double width;
+
+  ProfileIcon({this.height=100,this.width=100});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Image.asset("lib/assets/icon.png",width: width,height: height,)
     );
   }
 }
