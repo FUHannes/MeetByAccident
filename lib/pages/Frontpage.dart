cb77acf1c7ddc138969d13e1a6062ab6c7e6bb33
Hannes_OnChuwi_lubuntu <hanneselias@gmail.com>
2020-06-22 21:26:05 +0200

    small tweaks

--- lib/pages/Frontpage.dart
+++ lib/pages/Frontpage.dart
@@ -8,17 +8,22 @@ class Frontpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
+      body: Stack(
         children: <Widget>[
           VideoBG(),
           Container(
             color: Theme.of(context).canvasColor.withAlpha(20),
           ),
           Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              ProfileRow(),
-            ]
-          ),
+            ProfileRow(),
+            Column(
+              children:  <Widget>[
+                
+              ],
+            )
+          ]),
         ],
       ),
     );
@@ -30,22 +35,48 @@ class ProfileRow extends StatelessWidget {
   Widget build(BuildContext context) {
     User me = UserHelper().me;
     return RawMaterialButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(40)),
+      ),
+      fillColor: Theme.of(context).backgroundColor.withAlpha(150),
       highlightColor: Theme.of(context).primaryColor.withAlpha(60),
-        onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context) => Settings()));
-        },
-        child: Container(
-      padding: EdgeInsets.all(40),
-      child: Row(
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Settings()));
+      },
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
           children: <Widget>[
-            ProfileIcon(me,radius: 30,),
-            SizedBox(width:10),
-            //TODO: real name
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            SizedBox(height:20),
+            Row(
               children: <Widget>[
-                Text(me.name,maxLines: 2, style: TextStyle(fontSize:22,color: Theme.of(context).textTheme.headline2.color,),),
-                Text("Profil / Einstellungen", style: TextStyle(fontSize:12,color: Theme.of(context).textTheme.headline2.color,),)
+                ProfileIcon(
+                  me,
+                  radius: 30,
+                ),
+                SizedBox(width: 10),
+                //TODO: real name
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      me.name,
+                      maxLines: 2,
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Theme.of(context).textTheme.headline1.color,
+                      ),
+                    ),
+                    Text(
+                      "Profil / Einstellungen",
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Theme.of(context).textTheme.headline2.color,
+                      ),
+                    )
+                  ],
+                ),
               ],
             ),
           ],
@@ -59,19 +90,17 @@ class ProfileIcon extends StatelessWidget {
   double radius;
   User user;
 
-  ProfileIcon(this.user,{this.radius=50});
+  ProfileIcon(this.user, {this.radius = 50});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:CircleAvatar(
-        radius: radius*1.1,
-        backgroundColor: Theme.of(context).backgroundColor,
         child: CircleAvatar(
-          radius: radius,
-          backgroundImage:user.profileImg,
-        )
-      )
-    );
+            radius: radius * 1.1,
+            backgroundColor: Theme.of(context).backgroundColor,
+            child: CircleAvatar(
+              radius: radius,
+              backgroundImage: user.profileImg,
+            )));
   }
 }
