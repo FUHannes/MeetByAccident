580b715cbd3e99cb60ad9d589fd1f67509666b6e
Hannes_OnChuwi_lubuntu <hanneselias@gmail.com>
2020-06-22 20:31:49 +0200

    settings on click

--- lib/pages/Frontpage.dart
+++ lib/pages/Frontpage.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:meetby_accident/Widgets/MainDrawer.dart';
 import 'package:meetby_accident/Widgets/Reuse/VideoBG.dart';
 import 'package:meetby_accident/other/Userhelper.dart';
+import 'package:meetby_accident/pages/Settings.dart';
 
 class Frontpage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        VideoBG(),
-        Container(
-          color: Theme.of(context).canvasColor.withAlpha(20),
-        ),
-        Column(
-          children: <Widget>[
-            ProfileRow(),
-          ]
-        ),
-      ],
+    return Scaffold(
+        body: Stack(
+        children: <Widget>[
+          VideoBG(),
+          Container(
+            color: Theme.of(context).canvasColor.withAlpha(20),
+          ),
+          Column(
+            children: <Widget>[
+              ProfileRow(),
+            ]
+          ),
+        ],
+      ),
     );
   }
 }
@@ -26,15 +29,27 @@ class ProfileRow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     User me = UserHelper().me;
-    return Container(
+    return RawMaterialButton(
+      highlightColor: Theme.of(context).primaryColor.withAlpha(60),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Settings()));
+        },
+        child: Container(
       padding: EdgeInsets.all(40),
       child: Row(
-        children: <Widget>[
-          ProfileIcon(me,radius: 30,),
-          SizedBox(width:10),
-          //TODO: real name
-          Text(me.name,maxLines: 2, style: TextStyle(fontSize:22,color: Theme.of(context).textTheme.headline2.color,),),
-        ],
+          children: <Widget>[
+            ProfileIcon(me,radius: 30,),
+            SizedBox(width:10),
+            //TODO: real name
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(me.name,maxLines: 2, style: TextStyle(fontSize:22,color: Theme.of(context).textTheme.headline2.color,),),
+                Text("Profil / Einstellungen", style: TextStyle(fontSize:12,color: Theme.of(context).textTheme.headline2.color,),)
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
