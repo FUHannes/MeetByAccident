1648dffabfb4f7a6e760292ae2f5298dda206238
OverDriveGain <manar.zaboub@gmail.com>
2020-07-06 15:38:52 +0200

    Add age, location, country to user helper.dart

--- lib/other/datahandler/Userhelper.dart
+++ lib/other/datahandler/Userhelper.dart
@@ -4,12 +4,20 @@ import 'package:flutter/material.dart';
 
 class User {
   String name;
+  int age;
+  String city;
+  String country;
+  String hobbies;
   ImageProvider profileImg;
   Color favoriteColor;
   String streamUrl;
 
   User(
     this.name, {
+    this.age,
+    this.country,
+    this.city,
+    this.hobbies,
     profileImg,
     this.favoriteColor,
     this.streamUrl='http://clips.vorwaerts-gmbh.de/VfE_html5.mp4'??'http://techslides.com/demos/sample-videos/small.mp4'??'http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4',
@@ -19,12 +27,16 @@ class User {
   }
 
   factory User.morph(User u,
-    {String name, 
+    {String name,
+    int age,
+    String country,
+    String city,
+    String hobbies,
     profileImg,
     Color favoriteColor,
     String streamUrl,
   }){
-    return User(name??u.name,profileImg: profileImg??u.profileImg,favoriteColor: favoriteColor??u.favoriteColor, streamUrl: streamUrl??u.streamUrl);
+    return User(name??u.name, age:age??u.age, country: country??u.country, city:city??u.city, hobbies:hobbies??u.hobbies,profileImg: profileImg??u.profileImg,favoriteColor: favoriteColor??u.favoriteColor, streamUrl: streamUrl??u.streamUrl);
   }
 }
 
@@ -50,6 +62,10 @@ class UserHelper {
     String name = _getRandomString(_rnd.nextInt(8) + 1);
     return User(
       name,
+      age: _rnd.nextInt(66) + 18,
+      country: _getRandomString(_rnd.nextInt(8) + 1),
+      city: _getRandomString(_rnd.nextInt(8) + 1),
+      hobbies: _getRandomString(_rnd.nextInt(8) + 1),
       profileImg: AssetImage("lib/assets/stock_profile.png"),
       favoriteColor: Color.fromARGB(
           255, _rnd.nextInt(255), _rnd.nextInt(255), _rnd.nextInt(255)),
